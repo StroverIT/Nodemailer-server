83a3c6357332e61a0187a8abc5f573aaf0d08359
StR0vErBG <xcreaw1@abv.bg>
2022-05-02 19:08:43 +0200

    Chaning to require bcs of import bug with google

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
-import express from "express"
-import http from "http"
+require("dotenv").config();
 
-import {sendEmail} from "./utils/email.js"
+const express = require("express")
+const http = require("http")
+const cors = require("cors")
+const bodyParser = require("body-parser")
+
+const sendEmail = require("./utils/email.js")
 
 const app = express()
+
 const port = process.env.PORT || 8080
 const server = http.createServer(app)
 
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.post("/contactUs", async (req, res, next) => {  
     try{
-  
+      console.log(req.body);
     const message = `от ${req.body.user} - ${req.body.message}`
-      await sendEmail( req.body.email, "emilzlatinov123@gmail.com", req.body.problem , message)
+      await sendEmail( req.body.email, "emilzlatinov123@gmail.com", req.body.subject , message)
     res.send({messageEN: "You have successfully sent the email"})
+
     }catch(e){
       if(e) console.log(e);
     res.send({messageEN: "Something went wrong!"})
