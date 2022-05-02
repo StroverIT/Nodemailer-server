8ac7a09f1c2c93d7f3716f4e25aee3102f793574
StR0vErBG <xcreaw1@abv.bg>
2022-05-02 19:08:46 +0200

    Chaning to require bcs of import bug with google

--- utils/email.js
+++ utils/email.js
@@ -1,12 +1,13 @@
-import nodemailer from "nodemailer"
-import {google} from "googleapis"
-
+const nodemailer = require("nodemailer");
+const {google} = require("googleapis")
 
 const oAuth2Client = new google.auth.OAuth2(process.env.CLIENT_ID, process.env.CLIENT_SECRET, process.env.REDIRECT_URI)
 oAuth2Client.setCredentials({refresh_token: process.env.REFRESH_TOKEN})
-export const sendEmail = async (fromEmail, email, subject, text) => {
+const sendEmail = async (fromEmail, email, subject, text) => {
   try {
-    const accessToken = await oAuth2Client.getAccessToken()
+
+    const accessToken = await oAuth2Client.getAccessToken();
+
 
     const transporter = nodemailer.createTransport({
       service: 'gmail',
@@ -16,11 +17,10 @@ export const sendEmail = async (fromEmail, email, subject, text) => {
        clientId: process.env.CLIENT_ID,
        clientSecret: process.env.CLIENT_SECRET,
        refreshToken: process.env.REFRESH_TOKEN,
-       accessToken: process.env.accessToken
+       accessToken: accessToken
       },
 
     });
-
     await transporter.sendMail({
       from: fromEmail,
       to: email,
@@ -33,3 +33,5 @@ export const sendEmail = async (fromEmail, email, subject, text) => {
     console.log(error);
   }
 };
+
+module.exports = sendEmail;
